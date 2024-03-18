d71cb37a29d6371b5e61a51ed8c9a750ca3225e0
sreehari112 <38272677+sreehari112@users.noreply.github.com>
2024-03-19 04:08:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,33 +25,3 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
-
-
-
-module "sample" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "ravisreehari11+sbx@gmail.com"
-    AccountName               = "sample-aft"
-    ManagedOrganizationalUnit = "sample"
-    SSOUserEmail              = "ravisreehari11+sbx@gmail.com"
-    SSOUserFirstName          = "sample"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sample"
-}
